576c95fa1219a0ab0d2746232600dc03586ba17f
Valtriaz <valtriaz.gmail.com>
2026-09-09 11:46:19 +0530

    network request handling

--- src/network.rs
+++ src/network.rs
@@ -1,9 +1,7 @@
 use reqwest::blocking::Client;
 
 pub fn fetch(url: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let client = Client::builder()
-        .user_agent("RIFT-CLI/0.1.0")
-        .build()?;
+    let client = Client::builder().user_agent("RIFT-CLI/0.1.0").build()?;
 
     let response = client.get(url).send()?.error_for_status()?;
 
